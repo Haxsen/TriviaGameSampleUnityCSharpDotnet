ce432a7154837016c85c8f40e74e8d48c74e2323
Hassaan Ali <hassaan.ali.lel@gmail.com>
2022-09-04 04:12:12 +0500

    Added OpenTdb configuration class. This class contains site data such as links etc.

---
+++ Assets/Scripts/Haxsen/OpenTdb/OpenTdbConfig.cs
@@ -0,0 +1,11 @@
+namespace Haxsen.OpenTdb
+{
+    public class OpenTdbConfig
+    {
+        public const string OPENTDB_API_URL_FORMAT = "https://opentdb.com/api.php?{0}";
+        public const string OPENTDB_API_GET_AMOUNT = "amount={1}";
+        public const string OPENTDB_API_GET_CATEGORY = "&category={2}";
+        public const string OPENTDB_API_GET_DIFFICULTY = "&difficulty={3}";
+        public const string OPENTDB_API_GET_TYPE = "&type={4}";
+    }
+}
