00c28ea0b142601f06542c833fe11199dec57c09
Hassaan Ali <hassaan.ali.lel@gmail.com>
2022-09-05 03:17:45 +0500

    Added color options for UI

---
+++ Assets/Scripts/Haxsen/ScriptableObjects/UIColorOptionsSO.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Haxsen.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "UIColorOptionsSO", menuName = "ScriptableObjects/UIColorOptions", order = 1)]
+    public class UIColorOptionsSO : ScriptableObject
+    {
+        [SerializeField] private Color correctColor = Color.green;
+        [SerializeField] private Color incorrectColor = Color.red;
+
+        public Color GetButtonColor(bool isCorrect) => isCorrect ? correctColor : incorrectColor;
+    }
+}
