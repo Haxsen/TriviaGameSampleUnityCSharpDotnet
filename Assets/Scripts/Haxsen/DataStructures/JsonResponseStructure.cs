7e8b90ac8aac34746eaa5f9b6d71aba1dceec983
Hassaan Ali <hassaan.ali.lel@gmail.com>
2022-09-05 03:17:00 +0500

    Added Data Structures for parsing JSON data

---
+++ Assets/Scripts/Haxsen/DataStructures/JsonResponseStructure.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Haxsen.DataStructures
+{
+    [System.Serializable]
+    public class JsonResponseStructure
+    {
+        public int response_code;
+        public List<QuestionStructure> results;
+    }
+}
---
+++ Assets/Scripts/Haxsen/DataStructures/QuestionStructure.cs
@@ -0,0 +1,13 @@
+﻿namespace Haxsen.DataStructures
+{
+    [System.Serializable]
+    public class QuestionStructure
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public string[] incorrect_answers;
+    }
+}
