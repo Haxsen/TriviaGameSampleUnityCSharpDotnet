ce69fac8aaa08979cd4483233e5ca3d73764bb8b
Hassaan Ali <hassaan.ali.lel@gmail.com>
2022-09-05 03:15:36 +0500

    Modified OpenTdbConfig to work with builder

--- Assets/Scripts/Haxsen/OpenTdb/OpenTdbConfig.cs
+++ Assets/Scripts/Haxsen/OpenTdb/OpenTdbConfig.cs
@@ -2,10 +2,10 @@ namespace Haxsen.OpenTdb
 {
     public class OpenTdbConfig
     {
-        public const string OPENTDB_API_URL_FORMAT = "https://opentdb.com/api.php?{0}";
-        public const string OPENTDB_API_GET_AMOUNT = "amount={1}";
-        public const string OPENTDB_API_GET_CATEGORY = "&category={2}";
-        public const string OPENTDB_API_GET_DIFFICULTY = "&difficulty={3}";
-        public const string OPENTDB_API_GET_TYPE = "&type={4}";
+        public const string OPENTDB_API_URL_FORMAT = "https://opentdb.com/api.php";
+        public const string OPENTDB_API_GET_AMOUNT = "amount={0}";
+        public const string OPENTDB_API_GET_CATEGORY = "category={0}";
+        public const string OPENTDB_API_GET_DIFFICULTY = "difficulty={0}";
+        public const string OPENTDB_API_GET_TYPE = "type={0}";
     }
 }
